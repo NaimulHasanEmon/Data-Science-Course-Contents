4a93effbeb4800a239f8f05a5f2d88ac8fb7bd6a
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-07-20 02:05:41 +0600

    Conclusion and Wrapping up House Pricing Project and main.py file updated

--- Machine Learning/scikit-learn/Project-Dhaka/main_new.py
+++ Machine Learning/scikit-learn/Project-Dhaka/main_new.py
@@ -8,11 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import root_mean_squared_error
-from sklearn.model_selection import cross_val_score
 
 MODEL_FILE = "housing_model.pkl"
 PIPELINE_FILE = "housing_pipeline.pkl"

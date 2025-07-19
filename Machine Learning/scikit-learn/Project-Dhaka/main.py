fabca3bea7064c810c3f4999e2e3492f31f80bcb
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-07-19 22:53:25 +0600

    Training ML Algorithms on Preprocessed data

--- Machine Learning/scikit-learn/Project-Dhaka/main.py
+++ Machine Learning/scikit-learn/Project-Dhaka/main.py
@@ -3,6 +3,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score
 
 import pandas as pd
 import numpy as np
@@ -52,3 +57,38 @@ full_pipeline = ColumnTransformer([
 housing_prepared = full_pipeline.fit_transform(housing)
 print(housing_prepared)
 print(housing_prepared.shape)
+
+# 7. Train different  models
+
+# Linear Regression Model
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+lin_preds = lin_reg.predict(housing_prepared)
+lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
+print(f'Linear Regression RMSE: {lin_rmse}')
+
+# cross-validation
+lin_rmse_cross = np.sqrt(-cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10))
+print(pd.Series(lin_rmse_cross).describe())
+
+# Decision Tree Regression Model
+dec_reg = DecisionTreeRegressor()
+dec_reg.fit(housing_prepared, housing_labels)
+dec_preds = dec_reg.predict(housing_prepared)
+dec_rmse = root_mean_squared_error(housing_labels, dec_preds)
+print(f'Decision Tree Regression RMSE: {np.sqrt((dec_rmse))}')
+
+# cross-validation
+tree_rmse_cross = np.sqrt(-cross_val_score(dec_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10))
+print(pd.Series(tree_rmse_cross).describe())
+
+# Random Forest Regression Model
+rf_reg = RandomForestRegressor()
+rf_reg.fit(housing_prepared, housing_labels)
+rf_preds = rf_reg.predict(housing_prepared)
+rf_rmse = root_mean_squared_error(housing_labels, rf_preds)
+print(f'Random Forest Regression RMSE: {rf_rmse}')
+
+# cross-validation
+rf_rmse_cross = np.sqrt(-cross_val_score(rf_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10))
+print(pd.Series(rf_rmse_cross).describe())

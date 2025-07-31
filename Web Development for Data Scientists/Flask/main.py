78481c4d8ba7cc28f4eb70ee926f99c5dc31e17d
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-08-01 05:09:21 +0600

    Static and Templates folder

--- Web Development for Data Scientists/Flask/main.py
+++ Web Development for Data Scientists/Flask/main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, Naimul!</p>"
+def hello_naimul():
+    return "<p>Hello, I'm Naimul!</p>"
+
+@app.route("/about")
+def about():
+    return render_template("index.html")       # connects a html page with the server
+
+@app.route("/contact")
+def contact():
+    return "<p>This is contact page</p>"
 
 app.run(debug=True)

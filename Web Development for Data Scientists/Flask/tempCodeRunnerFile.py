ccc59f8a49441c9e0b99fdb4211118c7f0063d17
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-08-01 17:16:20 +0600

    Changing Static Path and Static Folders

--- Web Development for Data Scientists/Flask/tempCodeRunnerFile.py
+++ Web Development for Data Scientists/Flask/tempCodeRunnerFile.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(
-    __name__,
-    static_folder="assets",
-    static_url_path="/files"
-    )
+app = Flask(__name__)
 
 @app.route("/")
 def hello_naimul():

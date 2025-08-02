2df386ed841eb7692ba55a3f9a2fd4d6b99bbb16
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-08-02 20:13:07 +0600

    Query Parameters

---
+++ Web Development for Data Scientists/Flask/Query Parameters/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.secret_key='your_secret_key'
+
+@app.route("/")
+def hello_world():
+    name = request.args.get("name", default="Unnamed")
+    lang = request.args.get("lang", default="Programming")
+    print(name, lang)
+    return render_template("index.html", lang=lang, name=name)
+
+app.run(debug=True)

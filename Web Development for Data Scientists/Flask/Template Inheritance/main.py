3d44c09157f5cd44fa7adef11932ba610605bf66
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-08-02 19:58:42 +0600

    Message Flashing in Flask

--- Web Development for Data Scientists/Flask/Template Inheritance/main.py
+++ Web Development for Data Scientists/Flask/Template Inheritance/main.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
+app.secret_key='your_secret_key'
 
 @app.route("/")
 def hello_world():
+    flash("Thank You!")
     return render_template("index.html")
 
 @app.route("/about")
 def about():
+    flash("Thanks for visiting about page!")
     return render_template("about.html")
 
 app.run(debug=True)

a237c4c03466212177d616a3cac2ca16cfdc1550
Emon <mustakimemon1272000@gmail.com>
2025-09-20 16:43:29 +0600

    Creating prompt for our RAG based system

--- RAG based AI Teaching Assistant/process_incoming.py
+++ RAG based AI Teaching Assistant/process_incoming.py
@@ -29,5 +29,15 @@ max_idx = similarities.argsort()[::-1][0:top_results]
 new_df = df.loc[max_idx]
 # print(new_df[["audio number", "id","text"]])
 
-for idx, item in new_df.iterrows():
-    print(idx, item["id"], item["audio number"], item["text"])
+prompt = f'''I am teaching IELTS reading, writing, speaking and listening course. Here are video subtitle chunks containing audio number which is the exact serial of the video number, the starting time in seconds, the ending time in seconds as well and the text at that time:
+
+{new_df[["id","audio number","start","end","text"]].to_json()}
+---------------------------------
+"{incoming_query}"
+User asked this question related to the video chunks, you have to answer where and how much content is taught in which video and at what timestamps and guide the user to go to that particular video. If user asks unrelated questions, tell him that you can only answer questions related to this course.
+'''
+
+with open("C:\Data Science Course\RAG based AI Teaching Assistant\Prompt\prompt.txt", "w") as f:
+    f.write(prompt)
+# for idx, item in new_df.iterrows():
+#     print(idx, item["id"], item["audio number"], item["text"])
